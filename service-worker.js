d56aaf73dda719a4170e72970ce6fc66951c55a4
Rizka0813 <145784401+Rizka0813@users.noreply.github.com>
2024-11-07 12:59:55 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
 const cacheName = 'cv-cache-v1';
 const cacheAssets = [
     '/',
@@ -41,3 +49,36 @@ self.addEventListener('fetch', e => {
         fetch(e.request).catch(() => caches.match(e.request))
     );
 });
+
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyBw8JOxP60nS2IiecizejPHsysl162ls18",
+    authDomain: "cvrizka-d2cac.firebaseapp.com",
+    projectId: "cvrizka-d2cac",
+    storageBucket: "cvrizka-d2cac.firebasestorage.app",
+    messagingSenderId: "950329874155",
+    appId: "1:950329874155:web:e83dcecc5d149b1e0e10ae",
+    measurementId: "G-1CSN1LQTR5"
+  });
+  
+  // Retrieve an instance of Firebase Messaging so that it can handle background
+  // messages.
+  const messaging = firebase.messaging();
+  
+  messaging.onBackgroundMessage((payload) => {
+    console.log(
+      "[firebase-messaging-sw.js] Received background message ",
+      payload
+    );
+    // Customize notification here
+    const notificationTitle = "Website Portofolio";
+    const notificationOptions = {
+      body: "Website Cv Rizka Yuniarti telah di aktifkan",
+      icon: "/foto/jaemin.jpg",
+    };
+  
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
